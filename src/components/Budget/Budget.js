1a59b2a6187e18c0312c9e89e29ff37b401d93d6
Alex <alexbean95@gmail.com>
2020-11-09 15:42:24 -0700

    up to step 6

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -10,24 +10,43 @@ import './Budget.css';
 
 import {connect} from "react-redux"
 
+import {requestUserData} from "../../redux/userReducer"
+
+import {requestBudgetData} from "../../redux/budgetReducer"
 
 class Budget extends Component {
 
+  componentDidMount() {
+    this.props.requestUserData()
+    this.props.requestBudgetData()
+  }
+
   render() {
-    const {loading} = this.props.budget
+    const {firstName, lastName} = this.props.user
+    const {loading, purchases, budgetLimit} = this.props.budget
     return (
       <Background>
         {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav 
+            firstName = {firstName}
+            lastName = {lastName}
+          />
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
-              <DisplayPurchases />
+              <DisplayPurchases 
+                purchases = {purchases}
+              />
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 
+                purchases = {purchases}
+                budgetLimit = {budgetLimit}
+              />
+              <Chart2 
+                purchases = {purchases}
+              />
             </div>
           </div>
         </div>
@@ -45,4 +64,4 @@ const mapStateToProps = state => state
 //   }
 // }
 
-export default connect(mapStateToProps)(Budget);
+export default connect(mapStateToProps, {requestUserData, requestBudgetData})(Budget);
